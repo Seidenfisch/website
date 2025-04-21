9b12be22f4dbad6fd8138d06b23d010db1e07054
Seiden <36819433+Seidenfisch@users.noreply.github.com>
2025-04-21 17:28:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,12 +17,56 @@ themeToggle.addEventListener('click', () => {
   }
 });
 
+// Advanced Lightbox Gallery with Navigation
 
-document.querySelectorAll('.tile img').forEach(img => {
-  img.addEventListener('click', () => {
-    const instance = basicLightbox.create(`
-      <img src="${img.dataset.full}" alt="${img.alt}" style="max-width: 90vw; max-height: 90vh;">
-    `);
+const galleries = {
+  bike1: [
+    'https://i.imgur.com/SmQ76BE.jpeg',
+    'https://i.imgur.com/SmQ76BE.jpeg', // Same images for demo
+    'https://i.imgur.com/SmQ76BE.jpeg'
+  ],
+  bike2: [
+    'https://i.imgur.com/TGrDQMS.jpeg',
+    'https://i.imgur.com/TGrDQMS.jpeg',
+    'https://i.imgur.com/TGrDQMS.jpeg'
+  ],
+};
+
+document.querySelectorAll('.tile').forEach(tile => {
+  tile.addEventListener('click', () => {
+    const galleryName = tile.dataset.gallery;
+    const images = galleries[galleryName];
+    let currentIndex = 0;
+
+    const showImage = (index) => {
+      return \`
+        <div style="position: relative; display: flex; flex-direction: column; align-items: center;">
+          <img src="\${images[index]}" style="max-width: 90vw; max-height: 90vh;">
+          <div style="margin-top: 10px;">
+            <button id="prev-button" style="margin-right: 10px;">⬅️ Prev</button>
+            <button id="next-button">Next ➡️</button>
+          </div>
+        </div>
+      \`;
+    };
+
+    let instance = basicLightbox.create(showImage(currentIndex), {
+      onShow: (instance) => {
+        instance.element().querySelector('#prev-button').onclick = () => {
+          currentIndex = (currentIndex - 1 + images.length) % images.length;
+          instance.element().innerHTML = showImage(currentIndex);
+          instance.element().querySelector('#prev-button').onclick = instance.options.onShow(instance);
+          instance.element().querySelector('#next-button').onclick = instance.options.onShow(instance);
+        };
+        instance.element().querySelector('#next-button').onclick = () => {
+          currentIndex = (currentIndex + 1) % images.length;
+          instance.element().innerHTML = showImage(currentIndex);
+          instance.element().querySelector('#prev-button').onclick = instance.options.onShow(instance);
+          instance.element().querySelector('#next-button').onclick = instance.options.onShow(instance);
+        };
+      }
+    });
+
     instance.show();
   });
 });
