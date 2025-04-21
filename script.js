ef755dbbeb02cc8dc9acf3ada5c46d3a86f54fa1
Seiden <36819433+Seidenfisch@users.noreply.github.com>
2025-04-21 17:35:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ themeToggle.addEventListener('click', () => {
   }
 });
 
-// Advanced Lightbox Gallery with Navigation
+// Lightbox Gallery with Next/Prev buttons
 
 const galleries = {
   bike1: [
     'https://i.imgur.com/SmQ76BE.jpeg',
-    'https://i.imgur.com/NAGfVSA.jpeg', // Same images for demo
+    'https://i.imgur.com/SmQ76BE.jpeg',
     'https://i.imgur.com/SmQ76BE.jpeg'
   ],
   bike2: [
@@ -38,35 +38,33 @@ document.querySelectorAll('.tile').forEach(tile => {
     const images = galleries[galleryName];
     let currentIndex = 0;
 
-    const showImage = (index) => {
-      return \`
-        <div style="position: relative; display: flex; flex-direction: column; align-items: center;">
-          <img src="\${images[index]}" style="max-width: 90vw; max-height: 90vh;">
-          <div style="margin-top: 10px;">
-            <button id="prev-button" style="margin-right: 10px;">⬅️ Prev</button>
-            <button id="next-button">Next ➡️</button>
-          </div>
+    const instance = basicLightbox.create(`
+      <div style="position: relative; display: flex; flex-direction: column; align-items: center;">
+        <img id="lightbox-image" src="${images[currentIndex]}" style="max-width: 90vw; max-height: 90vh;">
+        <div style="margin-top: 10px;">
+          <button id="prev-button" style="margin-right: 10px;">⬅️ Prev</button>
+          <button id="next-button">Next ➡️</button>
         </div>
-      \`;
-    };
-
-    let instance = basicLightbox.create(showImage(currentIndex), {
-      onShow: (instance) => {
-        instance.element().querySelector('#prev-button').onclick = () => {
-          currentIndex = (currentIndex - 1 + images.length) % images.length;
-          instance.element().innerHTML = showImage(currentIndex);
-          instance.element().querySelector('#prev-button').onclick = instance.options.onShow(instance);
-          instance.element().querySelector('#next-button').onclick = instance.options.onShow(instance);
-        };
-        instance.element().querySelector('#next-button').onclick = () => {
-          currentIndex = (currentIndex + 1) % images.length;
-          instance.element().innerHTML = showImage(currentIndex);
-          instance.element().querySelector('#prev-button').onclick = instance.options.onShow(instance);
-          instance.element().querySelector('#next-button').onclick = instance.options.onShow(instance);
-        };
-      }
-    });
+      </div>
+    `);
 
     instance.show();
+
+    setTimeout(() => {
+      const updateImage = () => {
+        const img = instance.element().querySelector('#lightbox-image');
+        img.src = images[currentIndex];
+      };
+
+      instance.element().querySelector('#prev-button').onclick = () => {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        updateImage();
+      };
+
+      instance.element().querySelector('#next-button').onclick = () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        updateImage();
+      };
+    }, 100);
   });
 });
